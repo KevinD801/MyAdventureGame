c17041fba9639afa57d2f3eccbfc88002a329583
Kevin Do <kevin.do2082@gmail.com>
2021-09-22 16:52:25 -0500

    New Project: MyAdventureGame

---
+++ MyAdventureGame/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyAdventureGame
+{
+    class Game
+    {
+        public void Run()
+        {
+
+        }
+    }
+}
